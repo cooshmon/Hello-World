a22400cdff0cca1e7ba616f589996ba17ac3b312
mattcoosh <mcuschie@risd.edu>
2021-10-15 13:01:07 -0400

    Create my.js

---
+++ my.js
@@ -0,0 +1,14 @@
+
+console.log ("check");
+
+
+$(document).ready(function(){
+ $("button").click(function(work){
+    $("p").toggle(); 
+});
+});
+console.log("work");
+$("button").click(function(){
+   $('button').toggleClass('darkmode');
+
+});console.log('dark mode');
